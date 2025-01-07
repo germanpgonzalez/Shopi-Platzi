b0ad9ac089b576a4581e0bcbbd8b475d78010cc8
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-01-07 11:05:43 -0300

    Agregando las cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
 
 export const Home = () => {
+
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+     .then(response => response.json())
+     .then(data => setProducts(data));
+  },[])
+
   return (
     <Layout>
-      <Card/>
+      <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+        {
+          products?.map((products) => (
+            <Card key={products.id} data={products} />
+          ))    
+        }
+      </div>
+
     </Layout>
   )
 }
